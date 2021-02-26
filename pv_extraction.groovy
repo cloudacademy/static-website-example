0d0c938256be3517ee7c4d20739834e7d9d6213e
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2021-02-26 12:46:21 +0530

    Update pv_extraction.groovy

--- pv_extraction.groovy
+++ pv_extraction.groovy
@@ -1,8 +1,10 @@
-
-
+def dbhostname
+def intDBhostname = "lpesipo1000int.schneider-electric.com:1521:pesv2int"
+def pprDBhostname = "lpesipo1009ppr.schneider-electric.com:1521:pesv2ppr"
+def prdDBhostname = "lpesipo1002prd.schneider-electric.com:1521:pesv2prd"
 def dbname(){
-    if ($ENVIRONMENT == 'INT'){
-        echo "Function Working"
+    if (params.ENVIRONMENT == 'INT'){
+        return 'lpesipo1000int.schneider-electric.com:1521:pesv2int'
     }
 }
 
@@ -18,7 +20,9 @@ pipeline {
             steps {
 		    script{
 		    	echo "Hello World"
-                dbname()
+		    	dbhostname = "${dbname()}"
+		    	echo "the DataBase is: ${dbhostname}"
+                // dbname()
 		    } 
             }
         }

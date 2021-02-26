7b63d27c780c41465b6544cbf244761253a23d9a
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2021-02-26 12:50:41 +0530

    Update pv_extraction.groovy

--- pv_extraction.groovy
+++ pv_extraction.groovy
@@ -1,10 +1,11 @@
 def dbhostname
-def intDBhostname = "lpesipo1000int.schneider-electric.com:1521:pesv2int"
-def pprDBhostname = "lpesipo1009ppr.schneider-electric.com:1521:pesv2ppr"
-def prdDBhostname = "lpesipo1002prd.schneider-electric.com:1521:pesv2prd"
 def dbname(){
     if (params.ENVIRONMENT == 'INT'){
         return 'lpesipo1000int.schneider-electric.com:1521:pesv2int'
+    }else if (params.ENVIRONMENT == 'PPR'){
+        return 'lpesipo1009ppr.schneider-electric.com:1521:pesv2ppr'
+    }else if (params.ENVIRONMENT == 'PRD'){
+        return 'lpesipo1002prd.schneider-electric.com:1521:pesv2prd'
     }
 }
 

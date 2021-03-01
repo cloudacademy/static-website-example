db1561504d15186bc0a591819950f17dce3e2a1c
techaspirant94 <76193700+techaspirant94@users.noreply.github.com>
2021-03-01 12:47:37 +0530

    Update pv_extraction.groovy

--- pv_extraction.groovy
+++ pv_extraction.groovy
@@ -28,7 +28,7 @@ pipeline {
     }
 
     stages {
-        stage("Desplay the Env variables") {
+        stage("Display the Env variables") {
             steps {
 		    script{
 		    	echo "Hello World"

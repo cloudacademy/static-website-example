60e29496c435470fe64f003f5789d3be9b11c80f
techaspirant94 <76193700+techaspirant94@users.noreply.github.com>
2021-03-01 13:00:41 +0530

    Update pv_extraction.groovy

--- pv_extraction.groovy
+++ pv_extraction.groovy
@@ -1,22 +1,27 @@
+import groovy.json.JsonOutput
+
 def dbhostname
 def destdb
 def dbname(){
     def env_info=["db_name":"","credentials_id":"","user_name":"","password":""]	
     if (params.ENVIRONMENT == 'INT'){
 	env_info["db_name"] = "int_database"  
-	def json = new groovy.json.JsonBuilder()
-	json rootKey: env_info     
-        return groovy.json.JsonOutput.prettyPrint(json.toString()) 
+	//def json = new groovy.json.JsonBuilder()
+	//json rootKey: env_info     
+        //return groovy.json.JsonOutput.prettyPrint(json.toString()) 
+	return JsonOutput.prettyPrint(JsonOutput.toJson(env_info))    
     }else if (params.ENVIRONMENT == 'PPR'){
         env_info["db_name"] = "PPR_database"  
-	def json = new groovy.json.JsonBuilder()
-	json rootKey: env_info     
-        return groovy.json.JsonOutput.prettyPrint(json.toString())
+	//def json = new groovy.json.JsonBuilder()
+	//json rootKey: env_info     
+        //return groovy.json.JsonOutput.prettyPrint(json.toString())
+	 return JsonOutput.prettyPrint(JsonOutput.toJson(env_info))    
     }else if (params.ENVIRONMENT == 'PRD'){
       	env_info["db_name"] = "PRD_database"  
-	def json = new groovy.json.JsonBuilder()
-	json rootKey: env_info     
-        return groovy.json.JsonOutput.prettyPrint(json.toString())
+	//def json = new groovy.json.JsonBuilder()
+	//json rootKey: env_info     
+        //return groovy.json.JsonOutput.prettyPrint(json.toString())
+	return JsonOutput.prettyPrint(JsonOutput.toJson(env_info))     
     }
 }
 def dbdestdb(){

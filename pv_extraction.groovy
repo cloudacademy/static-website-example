32503b4529b85f853186921ea876a985cd6f99e5
techaspirant94 <76193700+techaspirant94@users.noreply.github.com>
2021-03-01 19:56:17 +0530

    Update pv_extraction.groovy

--- pv_extraction.groovy
+++ pv_extraction.groovy
@@ -1,34 +1,45 @@
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 
+
 def dbhostname
 def destdb
 def dbname(){
     def env_info=["db_name":"","credentials_id":"","user_name":"","password":""]	
     if (params.ENVIRONMENT == 'INT'){
 	env_info["db_name"] = "int_database"
-	env_info["credentials_id"] = "int_credentials_id"
+	env_info["credentials_id"] = "5e3e48a8-651d-48bd-813c-a3735c8af832"
 	env_info["user_name"] = "int_user_name"
 	env_info["password"] = "int_password"    
-	return JsonOutput.prettyPrint(JsonOutput.toJson(env_info))     
+	//return JsonOutput.prettyPrint(JsonOutput.toJson(env_info))     
     }else if (params.ENVIRONMENT == 'PPR'){
         env_info["db_name"] = "ppr_database"
 	env_info["credentials_id"] = "ppr_credentials_id"
 	env_info["user_name"] = "ppr_user_name"
 	env_info["password"] = "ppr_password"  
-	return JsonOutput.prettyPrint(JsonOutput.toJson(env_info))     
+	//return JsonOutput.prettyPrint(JsonOutput.toJson(env_info))     
     }else if (params.ENVIRONMENT == 'PRD'){
       	env_info["db_name"] = "prd_database"
 	env_info["credentials_id"] = "prd_credentials_id"
 	env_info["user_name"] = "prd_user_name"
 	env_info["password"] = "prd_password"   
-	return JsonOutput.prettyPrint(JsonOutput.toJson(env_info))        
+//	return JsonOutput.prettyPrint(JsonOutput.toJson(env_info))        
     }
+    return JsonOutput.prettyPrint(JsonOutput.toJson(env_info)) 
 }
 def dbdestdb(){
     if (params.ENVIRONMENT == 'INT'){
         destdb = 'kaasmongo'
     }
+}
+
+def get(key){
+    info="${dbname()}"
+    jsonSlurper = new JsonSlurper()
+    info_object = jsonSlurper.parseText(info)
+    echo key
+    echo info_object.key
+    return info_object.key
 }
 
 
@@ -43,17 +54,20 @@ pipeline {
         stage("Display the Env variables") {
             steps {
 		    script{
+                
+				//def info_object =new groovy.json.JsonSlurperClassic().parseText(info)
+				//String credential_id = info_object.credentials_id.toString()
+				echo "${get(credential_id)}"
+				withCredentials([usernamePassword(credentialsId: get(credential_id), passwordVariable: 'CATA_PASS', usernameVariable: 'CATA_USER')])
+				{
 		  
-		    	def info="${dbname()}"
-			def jsonSlurper = new JsonSlurper()
-     			def info_object = jsonSlurper.parseText(info) 
-			echo info_object.toString()        
-		    	echo "the DataBase is: ${info_object.db_name}"
-			echo "the DataBase is: ${info_object.credentials_id}"
-			echo "the DataBase is: ${info_object.user_name}" 
-			echo "the DataBase is: ${info_object.password}"     
-			// dbname()
-			//echo "The Destination DB is : ${destdb}"
+						
+						echo info_object.toString()
+						echo "the DataBase is: ${info_object.credentials_id}"
+						echo "the DataBase is: ${CATA_PASS}" 
+						echo "the DataBase is: ${CATA_USER}"     
+					
+				}
 		    } 
             }
         }

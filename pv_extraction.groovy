3a1281fa86071d24c00d5a4cd9d95d895e5b03ed
techaspirant94 <76193700+techaspirant94@users.noreply.github.com>
2021-03-01 12:24:10 +0530

    Update pv_extraction.groovy

--- pv_extraction.groovy
+++ pv_extraction.groovy
@@ -1,20 +1,15 @@
 def dbhostname
 def destdb
 def dbname(){
-    def env_info={
-		db_name='',
-		credentials_id='',
-		user_name='',
-		password=''
-	}	
+    def env_info=[db_name='',credentials_id='',user_name='',password='']	
     if (params.ENVIRONMENT == 'INT'){
-	env_info.db_name = 'int_database'        
+	env_info["db_name"] = 'int_database'        
         return env_info
     }else if (params.ENVIRONMENT == 'PPR'){
-        env_info.db_name = 'PPR_database'        
+        env_info["db_name"] = 'PPR_database'        
         return env_info
     }else if (params.ENVIRONMENT == 'PRD'){
-        env_info.db_name = 'PRD_database'        
+        env_info["db_name"] = 'PRD_database'        
         return env_info
     }
 }
@@ -38,7 +33,7 @@ pipeline {
 		    script{
 		    	echo "Hello World"
 		    	dbhostname = "${dbname()}"
-		    	echo "the DataBase is: ${dbhostname}"
+		    	echo "the DataBase is: ${dbhostname["db_name"]}"
                 // dbname()
                 echo "The Destination DB is : ${destdb}"
 		    } 

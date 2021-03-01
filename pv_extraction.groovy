92b319d824f2e0fbde52f6af262683557ea2fc98
techaspirant94 <76193700+techaspirant94@users.noreply.github.com>
2021-03-01 12:55:52 +0530

    Update pv_extraction.groovy

--- pv_extraction.groovy
+++ pv_extraction.groovy
@@ -3,14 +3,20 @@ def destdb
 def dbname(){
     def env_info=["db_name":"","credentials_id":"","user_name":"","password":""]	
     if (params.ENVIRONMENT == 'INT'){
-	env_info["db_name"] = "int_database"        
-        return env_info
+	env_info["db_name"] = "int_database"  
+	def json = new groovy.json.JsonBuilder()
+	json rootKey: env_info     
+        return groovy.json.JsonOutput.prettyPrint(json.toString()) 
     }else if (params.ENVIRONMENT == 'PPR'){
-        env_info["db_name"] = "PPR_database"        
-        return env_info
+        env_info["db_name"] = "PPR_database"  
+	def json = new groovy.json.JsonBuilder()
+	json rootKey: env_info     
+        return groovy.json.JsonOutput.prettyPrint(json.toString())
     }else if (params.ENVIRONMENT == 'PRD'){
-        env_info["db_name"] = "PRD_database"        
-        return env_info
+      	env_info["db_name"] = "PRD_database"  
+	def json = new groovy.json.JsonBuilder()
+	json rootKey: env_info     
+        return groovy.json.JsonOutput.prettyPrint(json.toString())
     }
 }
 def dbdestdb(){
@@ -33,10 +39,7 @@ pipeline {
 		    script{
 		    	echo "Hello World"
 		    	def info="${dbname()}"
-			def json = new groovy.json.JsonBuilder()
-			json rootKey: info
-			println groovy.json.JsonOutput.prettyPrint(json.toString())    
-    			//echo info    
+			echo info    
 		    	//echo "the DataBase is: ${env_info["db_name"]}"
 			// dbname()
 			echo "The Destination DB is : ${destdb}"

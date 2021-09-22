d84529539767d1345e2eaea712ecbefc9cfb6a59
Norbert Langner <norbitor@gmail.com>
2021-09-22 22:44:45 +0200

    Delete all git metadata

--- script/build-prod.sh
+++ script/build-prod.sh
@@ -4,4 +4,4 @@ npm run build
 rm -rf assets/sass
 rm assets/js/main.js assets/js/util.js
 rm -rf node_modules
-rm .gitignore LICENSE.MD README.MD package*.json
+rm -rf LICENSE.MD README.MD package*.json .git*
